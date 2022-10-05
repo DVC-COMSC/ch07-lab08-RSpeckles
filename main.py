019456315d2f8169b9f6cfef0efe517a99e33730
RSpeckles <rchenspeckman@gmail.com>
2022-10-05 13:51:32 -0700

    Completed lab

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 # ******************************
 # Make your Code
 # ******************************
+userInputs = input("Enter some numbers: ")
+numbers = list(userInputs.split())
+
+for i in range(len(numbers)):
+    numbers[i] = int(numbers[i])
+
+num = int(input("Enter another number: "))
+
+for i in range(5):
+    if numbers[i] > num:
+        numbers.insert(i, num)
+        flag = 1
+        break
+
+if flag == 0:
+    numbers.append(num)
+
+print(numbers)
